9bf49d9682e8eface47e23170df3b63af5b359f8
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-07 20:11:22 +0900

    learning

---
+++ src/components/layouts/PublicHeader.tsx
@@ -0,0 +1,41 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function PublicHeader() {
+  return (
+    <div>
+      <header className="border-b bg-blue-200">
+        <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem asChild>
+                <Link href="/" className="font-bold text-xl">
+                  Blog
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className="flex items-center gap-4">
+          <Input
+            placeholder="記事を検索..."
+            className="w-[200px] ld:w-[300px]"
+          ></Input>
+          <Button variant="outline" asChild>
+            <Link href="/login">ログイン</Link>
+          </Button>
+          <Button variant="outline" asChild>
+            <Link href="/register">登録</Link>
+          </Button>
+        </div>
+      </header>
+    </div>
+  );
+}
