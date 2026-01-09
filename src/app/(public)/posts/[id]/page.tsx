f6d6171bc07890f0b67f911224b2117507178eef
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-09 19:42:11 +0900

    learning

---
+++ src/app/(public)/posts/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { getPost } from "@/lib/post";
+import { notFound } from "next/navigation";
+
+type Params = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function PostPage({ params }: Params) {
+  const { id } = await params;
+  const post = getPost(id);
+  if (!post) {
+    notFound();
+  }
+
+  return <div></div>;
+}
