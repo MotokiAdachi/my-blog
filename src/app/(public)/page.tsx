08e4edeb89de60a15f956d90095e7343f5907596
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-09 19:32:29 +0900

    learning

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,18 @@
-export default function PostPage() {
-  return <div>page</div>;
+import { getPosts } from "@/lib/post";
+import PostCard from "@/components/post/PostCared";
+import { Post } from "@/types/post";
+
+export default async function PostPage() {
+  const posts = await getPosts();
+  return (
+    <>
+      <div className="container mx-auto px-4py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
