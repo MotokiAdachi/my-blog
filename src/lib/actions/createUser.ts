3d7e0574e29ee719cf3e1695ace32c71e199b174
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-11 15:15:59 +0900

    learning

--- src/lib/actions/createUser.ts
+++ src/lib/actions/createUser.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib//prisma";
 import bcryptjs from "bcryptjs";
 import { signIn } from "@/auth";
 import { redirect } from "next/navigation";
+import { ZodError } from "zod";
 
 type ActionState = {
   success: boolean;
@@ -11,15 +12,17 @@ type ActionState = {
 };
 
 // バリデーションエラー処理
-function handleValidationError(error: any): ActionState {
+function handleValidationError(error: ZodError): ActionState {
   const { fieldErrors, formErrors } = error.flatten();
+  const castedFieldErrors = fieldErrors as Record<string, string[]>;
+
   if (formErrors.length > 0) {
     return {
       success: false,
       errors: { ...fieldErrors, confirmPassword: formErrors },
     };
   }
-  return { success: false, errors: fieldErrors };
+  return { success: false, errors: castedFieldErrors };
 }
 
 // カスタムエラー処理
