0e7eb30da1597429d8abaf5add6d1610736532de
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-11 14:07:32 +0900

    learning

---
+++ src/components/layouts/PrivateHeader.tsx
@@ -0,0 +1,27 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+export default async function PrivateHeader() {
+  return (
+    <header className="border-b bg-blue-200">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/dashboard" passHref>
+                <NavigationMenuLink className="font-bold text-xl">
+                  管理ページ
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </header>
+  );
+}
