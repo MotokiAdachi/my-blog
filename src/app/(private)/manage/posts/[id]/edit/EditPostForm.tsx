210578a28334c1a07c6d37fa212c6b4093cd4b71
MotokiAdachi <m.0622.kiki@gmail.com>
2026-01-16 17:29:47 +0900

    learning

--- src/app/(private)/manage/posts/[id]/edit/EditPostForm.tsx
+++ src/app/(private)/manage/posts/[id]/edit/EditPostForm.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { createPost } from "@/lib/actions/createPost";
 import Image from "next/image";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 type EditPostFormProps = {
   post: {
@@ -143,6 +144,20 @@ export default function EditPostForm({ post }: EditPostFormProps) {
             </ReactMarkdown>
           </div>
         )}
+        <RadioGroup
+          value={published.toString()}
+          name="published"
+          onValueChange={(value) => setPublished(value === "true")}
+        >
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="true" id="published-one" />
+            <Label htmlFor="published-one">表示</Label>
+          </div>
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="false" id="published-two" />
+            <Label htmlFor="published-two">非表示</Label>
+          </div>
+        </RadioGroup>
         <Button
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded"
